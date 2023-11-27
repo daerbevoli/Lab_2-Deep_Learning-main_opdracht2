9a9cdfda30859c0318f5d9c75f03f9a8b4ee36c3
daerbevoli <sameer.baruwal@student.uantwerpen.be>
2023-11-27 11:50:15 +0100

    this file is complete

--- losses/losses.py
+++ losses/losses.py
@@ -5,6 +5,7 @@ import utilities.utils as utils
 
 def mse(input_tensor: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     """TODO: implement this method"""
+    # TENSORS
     # The mean should be all the elements in the amount of dimensions that you have
     return torch.mean((target - input_tensor) ** 2)
 
