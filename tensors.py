19894780d14b176a512b9435363c6919a31e4a6a
daerbevoli <sameer.baruwal@student.uantwerpen.be>
2023-11-27 11:38:30 +0100

    This file is complete and finished

--- tensors.py
+++ tensors.py
@@ -3,6 +3,7 @@ import torch
 from options.options import Options
 from utilities.utils import plot_tensor, mse, init_pytorch, not_implemented
 
+# TENSORS
 
 def create_image(options: Options) -> torch.Tensor:
     """
@@ -12,10 +13,8 @@ def create_image(options: Options) -> torch.Tensor:
     rgb = [(0.5021, 0.1138, 0.9047), (0.2843, 0.0684, 0.6829), (0.1935, 0.5483, 0.3117),
            (0.8017, 0.8733, 0.6258), (0.5914, 0.6004, 0.2893), (0.7038, 0.5983, 0.9914)]
 
-    # QUESTION
-    # What is the point of this? Why was this mentioned?
-    # What is meant with 'use them to separate the problem into smaller sections'?
-    # The function is not even used?
+    # Used to test knowledge of tensors
+    # not able to implement (yet)
 
     tt = torch.FloatTensor(rgb).to(options.device)
     return tt
